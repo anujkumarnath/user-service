cbaf7c3a4ed006c812b60497178c0430b90b1f20
Anuj <anujkumarnath@gmail.com>
2024-05-26 11:58:15 +0530

    added body parsers mw to express app

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const requestLogger = require('./request-logger');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(8000, () => {
 	console.log(banner);
 });
